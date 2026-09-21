661aa8f17cf5fbe17e726e48dcca802757972e51
Vitalii Poliakov <95705028+vitalik211287@users.noreply.github.com>
2026-09-21 21:20:40 +0300

    style: refine app background and catalog navigation icon

--- src/shared/components/AppNavigation/NavigationIcons.jsx
+++ src/shared/components/AppNavigation/NavigationIcons.jsx
@@ -1,7 +1,12 @@
 import Icon from "../Icon/Icon.jsx";
 
 export const HomeIcon = () => <Icon name="home" />;
-export const CatalogIcon = () => <Icon name="book" />;
+export const CatalogIcon = () => (
+  <svg viewBox="0 0 24 24" fill="none" aria-hidden="true">
+    <path d="M3.5 5.5h6.2c1.3 0 2.3 1 2.3 2.3V20c-.7-1-1.6-1.5-2.8-1.5H3.5v-13Z" />
+    <path d="M12 7.8c0-1.3 1-2.3 2.3-2.3h6.2v13h-5.7c-1.2 0-2.1.5-2.8 1.5V7.8Z" />
+  </svg>
+);
 export const AddIcon = () => <Icon name="add" />;
 export const CalendarIcon = () => <Icon name="calendar" />;
 export const ReaderIcon = () => <Icon name="reading" />;
